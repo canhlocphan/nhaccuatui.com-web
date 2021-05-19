a2d36804848c4d544d967904131b822f1acaefd7
canhlocphan <canhloc.phan@m.citynow.vn>
2021-05-19 09:40:41 +0700

    chia router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+// libs
 import React from "react";
-import "./App.css";
-import Home from "./pages/home";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// routers
+import routes from "./routers/appRouter";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Redirect from="/nhaccuatui.com-web" to="/" exact />
+      {routes.map(({ id, path, component }) => (
+        <Route key={id} path={path} component={component} exact></Route>
+      ))}
+    </Switch>
+    <Footer />
+  </Router>
+);
 
 export default App;
---
+++ src/routers/appRouter.js
@@ -0,0 +1,18 @@
+// components
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+const routes = [
+  {
+    id: 1,
+    path: "/",
+    component: Home,
+  },
+  {
+    id: 2,
+    path: "/about",
+    component: About,
+  },
+];
+
+export default routes;
