eeeba95c210d37033b730484d192981924dbf489
canhlocphan <canhloc.phan@m.citynow.vn>
2021-05-19 08:58:35 +0700

    divide-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+// libs
 import React from "react";
-import "./App.css";
-import Home from "./pages/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+// routers
+import ROUTER from "./routers/AppRouter";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      {ROUTER.map((id, path, component) => (
+        <Route key={id} path={path} component={component} exact></Route>
+      ))}
+    </Switch>
+    <Footer />
+  </Router>
+);
 
 export default App;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,17 @@
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+const ROUTER = [
+  {
+    id: 1,
+    path: "/home",
+    component: Home,
+  },
+  {
+    id: 2,
+    path: "/about",
+    component: About,
+  },
+];
+
+export default ROUTER;
