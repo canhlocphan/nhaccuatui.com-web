7fc17d1719a0e4382546fea6f5770076ec8b4b8b
canhlocphan <canhloc.phan@m.citynow.vn>
2021-05-19 09:06:39 +0700

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,38 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// pages
-import About from "./pages/About";
-import Home from "./pages/Home";
-
 // components
-import Header from "./components/header";
-import Footer from "./components/footer";
-
+import App from "./App";
 // others
 import "./index.scss";
 import "antd/dist/antd.css";
 
-ReactDOM.render(
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/nhaccuatui.com-web" exact>
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-    </Switch>
-    <Footer />
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App></App>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
